c3d53713ea3d16ab98a3591adf791231e74e01f7
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-03-15 08:26:28 -0300

    Add docstrings

--- pass_encrypt/pass_encrypt.py
+++ pass_encrypt/pass_encrypt.py
@@ -8,14 +8,33 @@ OUTPUT_FILE = "password.txt"
 
 
 def generate_salt():
+    """
+    Generates a random salt string.
+
+    Return:
+        str: A random salt
+    """
     return "".join(secrets.choice(ascii_letters) for _ in range (SALT_LENGTH))
 
 
 def hash_password(password, salt):
+    """
+    Hashes the given password with the provided salt using SHA-256.
+
+    Args:
+        password (str): The user's password.
+        salt (str): A random salt.
+
+    Return:
+        str: Hexadecimal representation of the hashed password.
+    """
     return sha256(password.encode() + salt.encode()).hexdigest()
 
 
 def main():
+    """
+    Interact with the user and store salt and hashed password in a file
+    """
     password = input("Enter password: ")
     salt = generate_salt()
     hashed_password = hash_password(password, salt)
