993f28522c4d1763fbdce3cba5fb7b716b6a2e21
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-05-27 20:27:23 -0300

    Minor changes

--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@
 # Quickly setup my configuration files on another system
 
 # Clone from here...
-GIT_REPO_URL="https://github.com/lucasherediadv/dotfiles.git"
+GIT_REPO_URL="https://github.com/lucasherediadv/dot.git"
 
 # to here
-DOTFILES_DIR="$HOME/.dotfiles"
+DOTFILES_DIR="$HOME/dot"
 
 # Create dotfiles directory if it dosen't exist
 mkdir "$DOTFILES_DIR"
 
 # Set git alias for dotfiles
-alias dotfiles='/usr/bin/git --git-dir=$DOTFILES_DIR/ --work-tree=$HOME'
+alias dot='/usr/bin/git --git-dir=$DOTFILES_DIR/ --work-tree=$HOME'
 
 # Source repository ignore the folder where you will clone it
 # so that you don't create weird recursion problems
@@ -23,8 +23,8 @@ git clone --bare "$GIT_REPO_URL" "$DOTFILES_DIR"
 
 # Checkout the actual content from the bare repository
 # -f will rewrite the already existing files
-dotfiles checkout -f
+dot checkout -f
 
 # Configure git to ignore untracked files
-dotfiles config status.showUntrackedFiles no
+dot config status.showUntrackedFiles no
 
